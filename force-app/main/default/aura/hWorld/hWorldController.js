f8fa28472a0950a5ba2723fb10e854c626783843
sajjanaml <laxmansajjanam24@gmail.com>
2022-04-01 15:55:42 -0400

    hMessageandhWorld

---
+++ force-app/main/default/aura/hWorld/hWorldController.js
@@ -0,0 +1,7 @@
+({
+    Clickme : function(component, event, helper) {
+    //    var c = component.set('v.message','Assigning a value to attribute in a component from Controller ');
+
+
+    }
+})
---
+++ force-app/main/default/aura/hWorldApp/hWorldApp.app
@@ -0,0 +1,4 @@
+<aura:application>
+    <c:hWorld/>
+   
+</aura:application>	
