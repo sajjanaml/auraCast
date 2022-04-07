ecb835225c6d2d20ed73ea9ea65da796e3118848
sajjanaml <laxmansajjanam24@gmail.com>
2022-04-07 09:24:39 -0400

    explore Application Event

---
+++ force-app/main/default/aura/siblingComponentOne/siblingComponentOneController.js
@@ -0,0 +1,12 @@
+({
+    handleSendData : function(component, event, helper) {
+        var greeting = component.get('v.greeting');
+        var appEvent =$A.get("e.c:exploreApplicationEvent");
+        appEvent.setParams({
+            message:greeting
+
+        });
+        appEvent.fire();
+
+    }
+})
---
+++ force-app/main/default/aura/siblingComponentTwo/siblingComponentTwoController.js
@@ -0,0 +1,8 @@
+({
+    handleApplicationEvent : function(component, event, helper) {
+        var greeting = event.getParam("message");
+
+        component.set('v.greeting',greeting );
+
+    }
+})
